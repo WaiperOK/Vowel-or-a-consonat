209fa74553e823776bd233c2de9708ed4c78d0d0
WaiperOK <86571941+WaiperOK@users.noreply.github.com>
2022-01-02 13:52:33 +0200

    Add files via upload

---
+++ perform.py
@@ -0,0 +1,7 @@
+ch = input("Enter a character: ")
+
+if(ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I'
+  or ch=='i' or ch=='o' or ch=='o' or ch=='u' or ch=='u'):
+     print(ch, 'is a Vowel')
+else:
+    print (ch, 'is a Consonant')
